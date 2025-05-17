4cbc5f4768bce4e32209418fcd5927c908daf24b
Gustavo Vital <gustavovital@id.uff.br>
2025-05-17 19:23:52 +0100

    update var montly and quarterly. finish estimations

--- VAR_montly.R
+++ VAR_montly.R
@@ -1,15 +1,18 @@
-# get DATA
+# PREVISÕES HÍBRIDAS DE INFLAÇÃO COM VAR, BVAR E VECM
+# Armazenando apenas p_livre e p_admin
+rm(list = ls())
+# Carregar dados e pacotes
 data_montly_d <- readRDS("data/data_montly_d.rds")
 data_montly_l <- readRDS("data/data_montly_l.rds")
 
-# libraries ====
 library(vars)
 library(BMR)
 library(tidyverse)
+library(forecast)
+library(urca)
+library(glue)
 
-#...............................................................................
-# Settings ----
-#...............................................................................
+# Definição de modelos e defasagens
 var_model_vars <- list(
   VAR_I     = c("p_livre", "p_admin", "brlx", "r"),
   VAR_II    = c("p_livre", "p_admin", "brlx", "selic", "pi_sa", "m1"),
@@ -30,62 +33,128 @@ var_model_lags <- list(
   VECM      = 1
 )
 
-#...............................................................................
-# Estimating models ----
-#...............................................................................
-for(vars in 1:length(var_model_vars)){
+# Inicializar base de dados para armazenar previsões
+forecast_df <- tibble()
+
+# Estimar modelos e armazenar previsões de p_livre e p_admin
+for (vars in 1:length(var_model_vars)) {
   
+  model_name <- names(var_model_vars)[vars]
+  message(glue("Estimating model: {model_name}"))
   
-  if(vars == 1 | vars == 2 | vars == 3){
-    print(paste('Estimating VAR:', vars))
-    data_var <- data_montly_d[c('date', unlist(var_model_vars[vars]))] %>%
+  if (model_name %in% c("VAR_I", "VAR_II", "VAR_III")) {
+    
+    data_var <- data_montly_d[c("date", var_model_vars[[vars]])] %>%
       column_to_rownames("date")
     
-    var_model <- VAR(data_var, p = unlist(var_model_lags[vars]), type = 'const')
-    forecast_var <- predict(var_model, n.ahead = 12)
+    var_model <- VAR(data_var, p = var_model_lags[[vars]], type = "const")
+    forecast_var <- predict(var_model, n.ahead = 12, ci = 0.95)
     
-  } else {
-    if(vars == 4 | vars == 5 | vars == 6){
-      print(paste('Estimating VAR:', vars))
-      data_var <- data_montly_d[c('date', unlist(var_model_vars[vars]))] %>%
-        column_to_rownames("date")
-      
-      vars_names <- names(data_var)
-      coef_prior <- sapply(vars_names[vars_names != "date"], function(name) {
-        as.numeric(arima(data_montly_d[[name]], order = c(1,0,0))$coef[1])
-      })
-      
-      Y <- data.matrix(na.omit(data_var))
-      
-      bvar_model <- new(bvarm)
-      bvar_model$build(
-        Y, intercept = TRUE, lags = unlist(var_model_lags[vars]))
-      
-      bvar_model$prior(coef_prior, 1, 1, 0.5, 0.5, 100, 1)
-      bvar_model$gibbs(10000)
-      
-      forecast_bvar <- forecast(
-        bvar_model, shocks = FALSE, 
-        var_names = colnames(Y), n_ahead = 12, back_data = 10,
-        plot=TRUE)
-      
-    } else{
-      print(paste('Estimating VAR:', vars))
-      data_var <- data_montly_l[c('date', unlist(var_model_vars[vars]))] %>%
-        column_to_rownames("date")
-      
-      apply(
-        data_var, 2, function(y) summary(
-          ur.df(y, type = "drift", selectlags = "AIC")))
-      
-      jotest <- ca.jo(data_var, type = "trace", ecdet = "const", K = 2)
-      summary(jotest)
-      
-      vecm_model <- cajorls(jotest, r = 2)
-      vec2var_model <- vec2var(jotest, r = 2)
-      
-      forecast_vec <- predict(vec2var_model, n.ahead = 12)
-      
+    for (v in names(forecast_var$fcst)) {
+      if (v %in% c("p_livre", "p_admin")) {
+        df_tmp <- tibble(
+          model     = model_name,
+          component = v,
+          horizon   = 1:12,
+          mean      = forecast_var$fcst[[v]][, 1],
+          lower     = forecast_var$fcst[[v]][, 2],
+          upper     = forecast_var$fcst[[v]][, 3]
+        )
+        forecast_df <- bind_rows(forecast_df, df_tmp)
       }
+    }
+    
+  } else if (model_name %in% c("BVAR_I", "BVAR_II", "BVAR_III")) {
+    
+    data_var <- data_montly_d[c("date", var_model_vars[[vars]])] %>%
+      column_to_rownames("date")
+    
+    vars_names <- names(data_var)
+    coef_prior <- sapply(vars_names, function(name) {
+      as.numeric(arima(data_montly_d[[name]], order = c(1, 0, 0))$coef[1])
+    })
+    
+    Y <- data.matrix(na.omit(data_var))
+    
+    bvar_model <- new(bvarm)
+    bvar_model$build(Y, intercept = TRUE, lags = var_model_lags[[vars]])
+    bvar_model$prior(coef_prior, 1, 1, 0.5, 0.5, 100, 1)
+    bvar_model$gibbs(10000)
+    
+    fcst <- BMR:::forecast.Rcpp_bvarm(
+      bvar_model, 
+      shocks = FALSE, 
+      var_names = colnames(Y), 
+      n_ahead = 12
+    )
+    
+    ci_fcst <- fcst$plot_vals
+    
+    for (j in 1:dim(ci_fcst)[3]) {
+      var_name <- colnames(Y)[j]
+      if (var_name %in% c("p_livre", "p_admin")) {
+        df_tmp <- tibble(
+          model     = model_name,
+          component = var_name,
+          horizon   = 1:12,
+          mean      = ci_fcst[1:12, 2, j],
+          lower     = ci_fcst[1:12, 1, j],
+          upper     = ci_fcst[1:12, 3, j]
+        )
+        forecast_df <- bind_rows(forecast_df, df_tmp)
+      }
+    }
+    
+  } else if (model_name == "VECM")  {
+    
+    data_var <- data_montly_l[c("date", var_model_vars[[vars]])] %>%
+      column_to_rownames("date")
+    
+    jotest <- ca.jo(data_var, type = "trace", ecdet = "const", K = 2)
+    vec2var_model <- vec2var(jotest, r = 2)  # modelo em nível
+    
+    forecast_vec <- predict(vec2var_model, n.ahead = 12, ci = 0.95)
+    
+        df_tmp <- tibble(
+          model     = model_name,
+          component = 'p_livre',
+          horizon   = 1:12,
+          mean      = diff(c(tail(data_montly_l$p_livre,1), forecast_vec$fcst[['p_livre']][, "fcst"])),
+          lower     = diff(c(tail(data_montly_l$p_livre,1), forecast_vec$fcst[['p_livre']][, "lower"])),
+          upper     = diff(c(tail(data_montly_l$p_livre,1), forecast_vec$fcst[['p_livre']][, "upper"]))
+        )
+        forecast_df <- bind_rows(forecast_df, df_tmp)
+      
+    
   }
 }
+
+
+component_I <- forecast_df %>%
+  filter(model %in% c('VAR_I', 'VAR_II', 'VAR_III', 'VECM')) %>%
+  group_by(component, horizon) %>%
+  summarise(
+    model = paste("COMPONENT I"),
+    mean  = median(mean, na.rm = TRUE),
+    lower = median(lower, na.rm = TRUE),
+    upper = median(upper, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  relocate(model)
+
+
+component_II <- forecast_df %>%
+  filter(model %in% c('BVAR_I', 'BVAR_II', 'BVAR_III')) %>%
+  group_by(component, horizon) %>%
+  summarise(
+    model = paste("COMPONENT II"),
+    mean  = median(mean, na.rm = TRUE),
+    lower = median(lower, na.rm = TRUE),
+    upper = median(upper, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  relocate(model)
+
+forecast_df <- bind_rows(forecast_df, component_I, component_II)
+
+saveRDS(forecast_df, file = "data/forecast_montly.rds")
---
+++ figures.R
@@ -0,0 +1,53 @@
+rm(list = ls())
+
+library(tidyverse)
+
+data_montly_d <- readRDS("data/data_montly_d.rds")
+forecast_m <- readRDS('data/forecast_montly.rds')
+forecast_q <- readRDS('data/forecast_quarter.rds')
+
+forecast_m_wide <- forecast_m %>%
+  pivot_wider(
+    names_from = model,
+    values_from = c(mean, lower, upper)
+  )
+
+forecast_q_wide <- forecast_q %>%
+  pivot_wider(
+    names_from = model,
+    values_from = c(mean, lower, upper)
+  )
+
+
+
+
+
+
+forecast_m_wide %>%
+  dplyr::filter(component == 'p_admin') %>% 
+  ggplot(aes(x = horizon)) +
+  
+  geom_line(aes(y = mean_VAR_I), size=.2, color = 'dodgerblue2') +
+  geom_line(aes(y = mean_VAR_II), size=.2, color = 'dodgerblue3') +
+  geom_line(aes(y = mean_VAR_III), size=.2, color = 'dodgerblue4') +
+  
+  geom_line(aes(y = mean_BVAR_I), size=.2, color = 'tomato2') + 
+  geom_line(aes(y = mean_BVAR_II), size=.2, color = 'tomato3') + 
+  geom_line(aes(y = mean_BVAR_III), size=.2, color = 'tomato4') +
+  geom_line(aes(y = mean_VECM), size=.2, color = 'darkgreen') +
+  
+  geom_line(aes(y = `mean_COMPONENT I`), color = "black", size = 0.8) +
+  geom_ribbon(aes(ymin = `lower_COMPONENT I`, ymax = `upper_COMPONENT I`),
+              fill = "black", alpha = 0.15) +
+  
+  # Component II
+  geom_line(aes(y = `mean_COMPONENT II`), color = "orange", size = 0.8) +
+  geom_ribbon(aes(ymin = `lower_COMPONENT II`, ymax = `upper_COMPONENT II`),
+              fill = "orange", alpha = 0.15) +
+  
+  labs(
+    title = "Forecasts for p_livre with Confidence Intervals",
+    y = "Forecasted Value",
+    x = "Horizon"
+  ) +
+  theme_minimal()
