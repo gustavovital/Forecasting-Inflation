1cd2b17a05c43dc44d4aa27a76f116dd7e862ee1
Gustavo Vital <gustavovital@id.uff.br>
2025-05-27 01:27:53 +0200

    Update figures.R

--- figures.R
+++ figures.R
@@ -16,27 +16,28 @@ forecast_compound <- readRDS('data/forecast_compound_diff.rds')
 
 # Graficos de Compound ====
 forecast_compound %>% 
-  mutate(line_width = ifelse(COMPOUND == "AVERAGE MODEL", 1, 0.1)) %>%
+  mutate(line_width = ifelse(COMPOUND == "AVERAGE MODEL", 1, 2)) %>%
   mutate(point_width = ifelse(COMPOUND == "AVERAGE MODEL", 1, 0.2)) %>%
+  mutate(alpha_width = ifelse(COMPOUND == "AVERAGE MODEL", 1, 0.05)) %>%
   ggplot(aes(x = as.Date(date), y = mean, 
              color = forecast_model, 
              group = interaction(forecast_model, COMPOUND))) +
-  geom_line(aes(size = line_width)) + 
+  geom_line(aes(size = line_width, alpha = alpha_width)) + 
   geom_point(aes(size = point_width)) +
   scale_size_identity() +  # This ensures the size values are used as-is
-  labs(title = "Forecast Model Performance",
+  labs(title = NULL,
        x = "Date", 
-       y = "Mean Value",
+       y = NULL,
        color = "Forecast Model") +
   theme_minimal() +
   theme(legend.position = "bottom",
-        plot.title = element_text(hjust = 0.5, face = "bold"))
+        plot.title = element_text(hjust = 0.5, face = "bold")) 
 
 
 
 # Supondo que `p_livre_df` tenha colunas: date, p_livre
-p_livre %>%
-  arrange(date) %>%
-  mutate(p_livre_real_acc = p_livre - lag(p_livre, 12)) 
-  
+# p_livre %>%
+#   arrange(date) %>%
+#   mutate(p_livre_real_acc = p_livre - lag(p_livre, 12)) 
+#   
   
