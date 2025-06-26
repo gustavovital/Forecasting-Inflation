3fe7cf20b11517aff35d66490812d4ac46eec023
Gustavo Vital <gustavovital@id.uff.br>
2025-06-26 13:43:27 +0100

    Create unit_root_tests.R

---
+++ unit_root_tests.R
@@ -0,0 +1,72 @@
+options(max.print = 100000)
+library(tidyverse)
+
+# get data ----
+COVID_data_statistic <- readRDS("data/COVID_data_statistic.rds")
+COVID_data_quarter_d <- readRDS("data/COVID_data_quarter_d.rds") %>%
+  filter(date >= as.Date("2012-01-01"))
+COVID_data_quarter_l <- readRDS("data/COVID_data_quarter_l.rds") %>%
+  filter(date >= as.Date("2012-01-01"))
+
+COVID_data_montly_d <- readRDS("data/COVID_data_montly_d.rds")
+COVID_data_montly_l <- readRDS("data/COVID_data_montly_l.rds")
+
+# a bit of wrangling ----
+series_names <- names(df)[!(names(df) %in% c("date", "D_COVID"))]
+
+# results
+unit_root_results_s <- COVID_data_statistic %>%
+  select(where(is.numeric)) %>%             
+  select(-D_COVID) %>%                      
+  imap_dfr(~ {
+    result <- unit_root_table(.x)
+    result$Variable <- .y
+    result
+  }) %>%
+  select(Variable, everything())   
+
+unit_root_results_qd <- COVID_data_quarter_d %>%
+  select(where(is.numeric)) %>%             
+  select(-D_COVID) %>%                      
+  imap_dfr(~ {
+    result <- unit_root_table(.x)
+    result$Variable <- .y
+    result
+  }) %>%
+  select(Variable, everything()) 
+
+unit_root_results_ql <- COVID_data_quarter_l %>%
+  select(where(is.numeric)) %>%             
+  select(-D_COVID) %>%                      
+  imap_dfr(~ {
+    result <- unit_root_table(.x)
+    result$Variable <- .y
+    result
+  }) %>%
+  select(Variable, everything())      
+
+unit_root_results_md <- COVID_data_montly_d %>%
+  select(where(is.numeric)) %>%             
+  select(-D_COVID) %>%                      
+  imap_dfr(~ {
+    result <- unit_root_table(.x)
+    result$Variable <- .y
+    result
+  }) %>%
+  select(Variable, everything())     
+
+unit_root_results_ml <- COVID_data_montly_l %>%
+  select(where(is.numeric)) %>%             
+  select(-D_COVID) %>%                      
+  imap_dfr(~ {
+    result <- unit_root_table(.x)
+    result$Variable <- .y
+    result
+  }) %>%
+  select(Variable, everything())            
+
+print(unit_root_results_md)
+print(unit_root_results_ml)
+print(unit_root_results_qd)
+print(unit_root_results_ql)
+print(unit_root_results_s)
